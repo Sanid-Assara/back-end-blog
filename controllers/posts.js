b4a79b73cb50435c1ae35f6798a8c5c776fdf4c5
Berkan <brkntrkylmz@gmail.com>
2024-07-24 09:53:05 +0200

    updated getPost and createPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,39 +1,78 @@
-import pg from "pg";
-const { Client } = pg;
+import Post from "../models/Post.js";
+// import pg from "pg";
+// const { Client } = pg;
 
-// 1- Homepage (this include Nav and footer) a list of post + get request to retrieve all the post //Erika
+//Erika
+// 1- Homepage (this include Nav and footer) a list of post + get request to retrieve all the post
 export const getPosts = async (req, res) => {
-res.send("getProducts Test");
+  res.send("<h1>If you see me, it means it works</h1>"); //Delete this when you start coding
+  // You code solution goes here!
 };
+//Erika
 
-// 2- Form to add new post and display post detail + Post request and get request posts/:id  //Berkan
+// CREATE POST BERKAN
 export const createPost = async (req, res) => {
   try {
-    const { author, title, content, cover } = req.body;
-  const client = new Client({connectionString: process.env.PG_URI});
-  client. connect();
+    const result = await Post.create(red.body);
+    res.status(200).json({message: "Post created", result});
+    // const { author, title, content, cover } = req.body;
 
-  const result = await client.query("INSERT INTO posts (author, title, content, cover) VALUES ($1, $2, $3, $4) RETURNING *;", [author, title, content, cover]);
-  client.end();
-  res.status(200).json(result.rows);
-  
-  } catch (error) { 
-console.log(error);
-res.status(500).send("Something went wrong")
-  }};
-
-
-// GET POST BERKKAN
-
-export const getPost = async (req, res) => {
-res.send("getProduct Test");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
 };
 
-// 3- Form to edit post and function to delete - put and delete request //Sanid
+
+// GET POST BERKAN
+export const getPost = async (req, res) => {
+
+const  {id} = req.params();
+
+try {
+  const result = await Post.findByPk({id});
+  res.status(200).json({message: "got worked", result});
+
+} catch (error) {
+  console.log(error);
+  res.status(500).send("Something went wrong");
+}
+};
+
+//Berkan
+
+//Sanad
+// 3- Form to edit post and function to delete - put and delete request
 export const updatePost = async (req, res) => {
-res.send("updateProduct Test");
- };
+  try {
+    const {
+      body: { author, title, content, cover, date },
+      params: { id },
+    } = req;
+    if (!author || !title || !content || !cover || !date)
+      return res.status(400).json({
+        error: "All field are required",
+      });
+    const post = await Post.findByPk(id);
+    if (!post) return res.status(404).json({ error: "Post not found" });
+    await post.update(req.body);
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const deletePost = async (req, res) => {
-res.send("deleteProduct Test");
+  try {
+    const {
+      params: { id },
+    } = req;
+    const user = await Post.findByPk(id);
+    if (!user) return res.status(404).json({ error: "Post not found" });
+    await user.destroy();
+    res.json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
+//Sanad
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 import express from "express";
-import { createPost, deletePost, getPost, getPosts, updatePost } from "./controllers/posts.js";
-
+import { postsRouter } from "./routers/postsRouter.js";
 const PORT = process.env.PORT ?? 5000;
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/posts", getPosts);
-app.post("/posts", createPost);
-app.get("/posts/:id", getPost);
-app.put("/posts:id", updatePost);
-app.delete("/posts:id", deletePost);
+app.use("/API/posts", postsRouter);
 
-
-// Server Listener
 app.listen(PORT, () => {
-  console.log(`The server is running at http://localhost:${PORT}/`);
+  console.log(`The server is running at http://localhost:${PORT}/API/posts/`);
 });
