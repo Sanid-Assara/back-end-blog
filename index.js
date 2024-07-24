7363f34fca0572253d28c618d0b7ff71c818c74b
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-07-24 15:02:55 +0200

    Added cors for react to fix error  in frontend error message No 'Access-Control-Allow-Origin'

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { postsRouter } from "./routers/postsRouter.js";
+import cors from "cors";
 
 const PORT = process.env.PORT ?? 5000;
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/API/posts", postsRouter);
